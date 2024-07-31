15ef12f4656d4fab25c2bd35f2ffd2e303bb6a1b
EmmaIsaac <emmanuel.i157@yahoo.com.ar>
2024-07-31 15:54:27 -0300

    add: Layout

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <>
+            <p>2024 | Todos los derechos reservados.</p>
+        </>
+    )
+}
+
+export default Footer;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import './Header.css';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <div>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/contacto">Contacto</Link>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default Header;
